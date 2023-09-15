f8c02983f12a00b470ca3490adfa6fb27fbda61d
FraRussotto <russottofrancesco1@gmail.com>
2023-09-15 17:12:32 +0200

    [ADD] JS - Pari o dispari

---
+++ Pari o dispari/js/script.js
@@ -0,0 +1,45 @@
+//1. Creo i prompt per l'utente e successivamente controllo che abbia inserito le parole correttamente
+
+const chooseUser = prompt('Scegli tra pari o dispari')
+
+let message = document.getElementById(output);
+
+if(chooseUser !== 'pari' && chooseUser !== 'dispari') console.log('Devi inserire "pari" o "dispari"')
+else{
+  const nUser =  parseInt(prompt('Scegli un numero tra 1 e 5'))
+
+//2. Inserisco un controllo per verificare se l'utente abbia inserito un numero e una stringa e successivamente che sia compreso tra 1 e 5.
+  if(isNaN(nUser)) console.log('Inserire un valore numerico')
+  if(nUser > 0 && nUser < 6){
+    console.log('Numero user -->' + ' ' + nUser);
+
+
+//4. Genero il numero del pc attraverso la funzione esterna "random"
+    const nPc = random();
+    console.log('Numero pc -->' + ' ' + nPc);
+
+//5. Sommo i numero di utente e pc
+    const somma = nUser + nPc
+    console.log(somma);
+  
+  
+    const PariDispari = control(somma)
+  
+    if(PariDispari) message.innerHTML = 'Hai vinto';
+    message.innerHTML = 'Ha vinto il Pc';
+  }
+}
+
+// Funzioni
+
+function random(nPc){
+  nPc = Math.ceil(Math.random() * 5)
+  return nPc;
+}
+
+function control(somma){
+  let result = true;
+  if(somma % 2 !== 0) result = false;
+
+  return result;
+}
